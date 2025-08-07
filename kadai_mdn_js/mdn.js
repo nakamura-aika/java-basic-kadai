e4dbe52deabb647f2dc3c581c8feeeced929a532
nakamura-aika <zhongcunaixia228@gmail.com>
2025-08-07 11:22:40 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,7 +1,7 @@
 let day = new Date();
 
 let year = day.getFullYear();
-let month = day.getMonth();
+let month = day.getMonth()+1;
 let d_ay = day.getDate();
 
 console.log(year + '年' + month + '月' + d_ay + '日');
